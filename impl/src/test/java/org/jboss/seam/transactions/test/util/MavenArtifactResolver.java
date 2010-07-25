cd9fbc38fc1b9716388bc410041df91c2651545d
Stuart Douglas <stuart.w.douglas@gmail.com>
2010-07-25 01:28:57 +0000

    update maven artifact resolver so that version numbers are inferred from the pom

--- impl/src/test/java/org/jboss/seam/transactions/test/util/MavenArtifactResolver.java
+++ impl/src/test/java/org/jboss/seam/transactions/test/util/MavenArtifactResolver.java
@@ -1,25 +1,46 @@
 package org.jboss.seam.transactions.test.util;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * Resolves a maven artifact present on the test classpath.
+ * 
+ * @author Stuart Douglas
+ * 
+ */
 public class MavenArtifactResolver
 {
-   private static final String LOCAL_MAVEN_REPO =
-         System.getProperty("user.home") + File.separatorChar +
-         ".m2" + File.separatorChar + "repository";
+   private static final String LOCAL_MAVEN_REPO = System.getProperty("user.home") + File.separatorChar + ".m2" + File.separatorChar + "repository";
 
-   public static File resolve(String groupId, String artifactId, String version)
+   public static File resolve(String groupId, String artifactId)
    {
-      return new File(LOCAL_MAVEN_REPO + File.separatorChar +
-            groupId.replace(".", File.separator) + File.separatorChar +
-            artifactId + File.separatorChar +
-            version + File.separatorChar +
-            artifactId + "-" + version + ".jar");
+      String classPath = System.getProperty("java.class.path");
+      // first look for an artefact from the repo
+      String pathString = groupId.replace('.', File.separatorChar) + File.separatorChar + artifactId;
+      String regex = "[^:]*" + Pattern.quote(pathString) + "[^:]*";
+      Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+      Matcher matches = p.matcher(classPath);
+
+      if (!matches.find())
+      {
+         // find a resource from the local build
+         String localResource = Pattern.quote("target" + File.separatorChar + artifactId);
+         regex = "[^:]*" + localResource + "[^:]*";
+         p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+         matches = p.matcher(classPath);
+         if (!matches.find())
+         {
+            throw new RuntimeException("Unable to find maven archive " + groupId + ":" + artifactId + " on the test classpath");
+         }
+      }
+      return new File(matches.group(0));
    }
 
    public static File resolve(String qualifiedArtifactId)
    {
       String[] segments = qualifiedArtifactId.split(":");
-      return resolve(segments[0], segments[1], segments[2]);
+      return resolve(segments[0], segments[1]);
    }
 }
